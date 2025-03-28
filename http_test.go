27d7be782db161714dcab8b6eab9a27f8b2cd388
Shogo Iwano <shiwano@gmail.com>
2025-03-29 04:42:45 +0900

    fix: Fix TestNewHTTP

--- http_test.go
+++ http_test.go
@@ -39,72 +39,72 @@ func TestNewHTTP(t *testing.T) {
 	var tests = map[string]struct {
 		req  *http.Request
 		res  *http.Response
-		want *zapdriver.HTTPPayload
+		want zapdriver.HTTPPayload
 	}{
 		"empty": {
 			nil,
 			nil,
-			&zapdriver.HTTPPayload{RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"RequestMethod": {
 			&http.Request{Method: "GET"},
 			nil,
-			&zapdriver.HTTPPayload{RequestMethod: "GET", RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RequestMethod: "GET", RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"Status": {
 			nil,
 			&http.Response{StatusCode: 404},
-			&zapdriver.HTTPPayload{Status: 404, RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{Status: 404, RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"UserAgent": {
 			&http.Request{Header: http.Header{"User-Agent": []string{"hello world"}}},
 			nil,
-			&zapdriver.HTTPPayload{UserAgent: "hello world", RequestSize: "21", ResponseSize: "0"},
+			zapdriver.HTTPPayload{UserAgent: "hello world", RequestSize: "21", ResponseSize: "0"},
 		},
 
 		"RemoteIP": {
 			&http.Request{RemoteAddr: "127.0.0.1"},
 			nil,
-			&zapdriver.HTTPPayload{RemoteIP: "127.0.0.1", RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RemoteIP: "127.0.0.1", RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"Referrer": {
 			&http.Request{Header: http.Header{"Referer": []string{"hello universe"}}},
 			nil,
-			&zapdriver.HTTPPayload{Referer: "hello universe", RequestSize: "21", ResponseSize: "0"},
+			zapdriver.HTTPPayload{Referer: "hello universe", RequestSize: "21", ResponseSize: "0"},
 		},
 
 		"Protocol": {
 			&http.Request{Proto: "HTTP/1.1"},
 			nil,
-			&zapdriver.HTTPPayload{Protocol: "HTTP/1.1", RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{Protocol: "HTTP/1.1", RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"RequestURL": {
 			&http.Request{URL: &url.URL{Host: "example.com", Scheme: "https"}},
 			nil,
-			&zapdriver.HTTPPayload{RequestURL: "https://example.com", RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RequestURL: "https://example.com", RequestSize: "0", ResponseSize: "0"},
 		},
 
 		"RequestSize": {
 			&http.Request{Body: io.NopCloser(strings.NewReader("12345"))},
 			nil,
-			&zapdriver.HTTPPayload{RequestSize: "5", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RequestSize: "5", ResponseSize: "0"},
 		},
 
 		"ResponseSize": {
 			nil,
 			&http.Response{Body: io.NopCloser(strings.NewReader("12345"))},
-			&zapdriver.HTTPPayload{ResponseSize: "5", RequestSize: "0"},
+			zapdriver.HTTPPayload{ResponseSize: "5", RequestSize: "0"},
 		},
 
 		"simple request": {
 			httptest.NewRequest("POST", "/", strings.NewReader("12345")),
 			nil,
-			&zapdriver.HTTPPayload{
+			zapdriver.HTTPPayload{
 				RequestSize:   "5",
 				RequestMethod: "POST",
 				RemoteIP:      "192.0.2.1:1234",
@@ -118,13 +118,13 @@ func TestNewHTTP(t *testing.T) {
 		"simple response": {
 			nil,
 			&http.Response{Body: io.NopCloser(strings.NewReader("12345")), StatusCode: 404},
-			&zapdriver.HTTPPayload{RequestSize: "0", ResponseSize: "5", Status: 404},
+			zapdriver.HTTPPayload{RequestSize: "0", ResponseSize: "5", Status: 404},
 		},
 
 		"request & response": {
 			&http.Request{Method: "POST", Proto: "HTTP/1.1"},
 			&http.Response{StatusCode: 200},
-			&zapdriver.HTTPPayload{RequestMethod: "POST", Protocol: "HTTP/1.1", Status: 200, RequestSize: "0", ResponseSize: "0"},
+			zapdriver.HTTPPayload{RequestMethod: "POST", Protocol: "HTTP/1.1", Status: 200, RequestSize: "0", ResponseSize: "0"},
 		},
 	}
 
